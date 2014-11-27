cf5ec2da1981a9a3ec31daa0968b6e59fdeacb91
luiarthur <luiarthur@yahoo.com>
2014-11-27 12:05:38 -0700

    yes

--- regressionHearts.R
+++ regressionHearts.R
@@ -7,17 +7,11 @@ Z <- lapply(as.list(1:N), simulate.a.round)
 # Get rounds where people don't start, and the suit in play is spades
 Y <- numeric(N)
 
-#x1 = I am 2nd
-#x2 = I am 3rd
-#x3 = I am 4th
-#x2 = suit in play is S
-#x3 = suit in play is H
-#x4 = suit in play is C
-#x5 = 
-#x6 = 
-#x7 =
-#x8 =
-
+# Model1: If I am the first player in the trick
+#x1 = card suit I play
+#x2 = card value I play (as factor)
+#x3 = # of cards in suit I hold
+#x4 = # of cards in suit others hold
 
 S <- lapply(Z,function(z) {
   out <- matrix(0,0,ncol(z))
