104851124077673c3299fd07bf46dff8eef3740c
luiarthur <luiarthur@yahoo.com>
2014-11-27 13:19:25 -0700

    yes

--- regressionHearts.R
+++ regressionHearts.R
@@ -1,3 +1,4 @@
+source("lrbind.R")
 source("rapply.R")
 source("settings.R")
 
@@ -15,4 +16,14 @@ Y <- numeric(N)
 #x4 = # of cards in suit others hold
 
 new.Z <- lapply(Z,move)
+X <- lrbind(new.Z)
+dim(X)
+
+temp <- apply(X,2,function(x) as.numeric(get.value(x)))
+y <- ifelse(temp[,5]==1,1,0)
+X1 <- temp[,1]
+X2 <- get.suit(X[,1])
+X3
+X4 <- apply(matrix(1:length(X2)),1,function(s)sum(get.suit(played[-(0:ifelse(s%%13==0,13,s%%13)),1])==X2[s]))
+
 
